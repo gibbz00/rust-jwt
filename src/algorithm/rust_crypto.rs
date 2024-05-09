84d328f31fb1413b4d6eff276fd7568a98d2c642
gibbz00 <gabrielhansson00@gmail.com>
2024-05-09 21:44:02 +0200

    run cargo fmt and fix pem file references

--- src/algorithm/rust_crypto.rs
+++ src/algorithm/rust_crypto.rs
@@ -2,19 +2,19 @@
 //! According to that organization, only hmac is safely implemented at the
 //! moment.
 
+use crate::algorithm::{AlgorithmType, SigningAlgorithm, VerifyingAlgorithm};
+use crate::error::Error;
+use crate::SEPARATOR;
 use digest::{
     block_buffer::Eager,
     consts::U256,
     core_api::{BlockSizeUser, BufferKindUser, CoreProxy, FixedOutputCore},
     generic_array::typenum::{IsLess, Le, NonZero},
-    HashMarker, Digest,
+    Digest, HashMarker,
 };
 use hmac::{Hmac, Mac};
-use std::marker::PhantomData;
-use crate::algorithm::{AlgorithmType, SigningAlgorithm, VerifyingAlgorithm};
-use crate::error::Error;
-use crate::SEPARATOR;
 use signature::{DigestSigner, DigestVerifier, SignatureEncoding};
+use std::marker::PhantomData;
 
 /// A trait used to make the implementation of `SigningAlgorithm` and
 /// `VerifyingAlgorithm` easier.
@@ -39,16 +39,16 @@ type_level_algorithm_type!(sha2::Sha384, AlgorithmType::Hs384);
 type_level_algorithm_type!(sha2::Sha512, AlgorithmType::Hs512);
 
 impl<D> SigningAlgorithm for Hmac<D>
-    where
-        D: CoreProxy + TypeLevelAlgorithmType,
-        D::Core: HashMarker
-        + BufferKindUser<BufferKind=Eager>
+where
+    D: CoreProxy + TypeLevelAlgorithmType,
+    D::Core: HashMarker
+        + BufferKindUser<BufferKind = Eager>
         + FixedOutputCore
         + digest::Reset
         + Default
         + Clone,
-        <D::Core as BlockSizeUser>::BlockSize: IsLess<U256>,
-        Le<<D::Core as BlockSizeUser>::BlockSize, U256>: NonZero,
+    <D::Core as BlockSizeUser>::BlockSize: IsLess<U256>,
+    Le<<D::Core as BlockSizeUser>::BlockSize, U256>: NonZero,
 {
     fn algorithm_type(&self) -> AlgorithmType {
         D::algorithm_type()
@@ -62,8 +62,11 @@ impl<D> SigningAlgorithm for Hmac<D>
     }
 }
 
-
-pub struct AsymmetricAuthentication<HashAlgo, SignatureScheme, S> (SignatureScheme, PhantomData<HashAlgo>, PhantomData<S>);
+pub struct AsymmetricAuthentication<HashAlgo, SignatureScheme, S>(
+    SignatureScheme,
+    PhantomData<HashAlgo>,
+    PhantomData<S>,
+);
 
 impl<HashAlgo, SignatureScheme, S> AsymmetricAuthentication<HashAlgo, SignatureScheme, S> {
     pub fn new(scheme: SignatureScheme) -> Self {
@@ -74,14 +77,16 @@ impl<HashAlgo, SignatureScheme, S> AsymmetricAuthentication<HashAlgo, SignatureS
 pub struct AsymmetricAuthenticationBuilder<H>(PhantomData<H>);
 
 impl<H> AsymmetricAuthenticationBuilder<H> {
-    pub fn build<G, S>(scheme: G) -> AsymmetricAuthentication::<H, G, S> {
+    pub fn build<G, S>(scheme: G) -> AsymmetricAuthentication<H, G, S> {
         AsymmetricAuthentication::new(scheme)
     }
 }
 
 macro_rules! type_level_Asymmetric_algorithm_type {
     ($hash_type: ty, $signature_scheme: ty, $output: ty, $algorithm_type: expr) => {
-        impl TypeLevelAlgorithmType for AsymmetricAuthentication<$hash_type, $signature_scheme, $output> {
+        impl TypeLevelAlgorithmType
+            for AsymmetricAuthentication<$hash_type, $signature_scheme, $output>
+        {
             fn algorithm_type() -> AlgorithmType {
                 $algorithm_type
             }
@@ -89,32 +94,82 @@ macro_rules! type_level_Asymmetric_algorithm_type {
     };
 }
 
-type_level_Asymmetric_algorithm_type!(sha2::Sha256, p256::ecdsa::SigningKey, p256::ecdsa::Signature, AlgorithmType::Es256);
-type_level_Asymmetric_algorithm_type!(sha2::Sha256, p256::ecdsa::VerifyingKey, p256::ecdsa::Signature, AlgorithmType::Es256);
+type_level_Asymmetric_algorithm_type!(
+    sha2::Sha256,
+    p256::ecdsa::SigningKey,
+    p256::ecdsa::Signature,
+    AlgorithmType::Es256
+);
+type_level_Asymmetric_algorithm_type!(
+    sha2::Sha256,
+    p256::ecdsa::VerifyingKey,
+    p256::ecdsa::Signature,
+    AlgorithmType::Es256
+);
 
-type_level_Asymmetric_algorithm_type!(sha2::Sha384, p384::ecdsa::SigningKey, p384::ecdsa::Signature, AlgorithmType::Es384);
-type_level_Asymmetric_algorithm_type!(sha2::Sha384, p384::ecdsa::VerifyingKey, p384::ecdsa::Signature, AlgorithmType::Es384);
+type_level_Asymmetric_algorithm_type!(
+    sha2::Sha384,
+    p384::ecdsa::SigningKey,
+    p384::ecdsa::Signature,
+    AlgorithmType::Es384
+);
+type_level_Asymmetric_algorithm_type!(
+    sha2::Sha384,
+    p384::ecdsa::VerifyingKey,
+    p384::ecdsa::Signature,
+    AlgorithmType::Es384
+);
 
 // TODO: Es512 once p521 is implemented
 
-type_level_Asymmetric_algorithm_type!(sha2::Sha256, rsa::pkcs1v15::SigningKey<sha2::Sha256>, rsa::pkcs1v15::Signature, AlgorithmType::Rs256);
-type_level_Asymmetric_algorithm_type!(sha2::Sha256, rsa::pkcs1v15::VerifyingKey<sha2::Sha256>, rsa::pkcs1v15::Signature, AlgorithmType::Rs256);
+type_level_Asymmetric_algorithm_type!(
+    sha2::Sha256,
+    rsa::pkcs1v15::SigningKey<sha2::Sha256>,
+    rsa::pkcs1v15::Signature,
+    AlgorithmType::Rs256
+);
+type_level_Asymmetric_algorithm_type!(
+    sha2::Sha256,
+    rsa::pkcs1v15::VerifyingKey<sha2::Sha256>,
+    rsa::pkcs1v15::Signature,
+    AlgorithmType::Rs256
+);
 
-type_level_Asymmetric_algorithm_type!(sha2::Sha384, rsa::pkcs1v15::SigningKey<sha2::Sha384>, rsa::pkcs1v15::Signature, AlgorithmType::Rs384);
-type_level_Asymmetric_algorithm_type!(sha2::Sha384, rsa::pkcs1v15::VerifyingKey<sha2::Sha384>, rsa::pkcs1v15::Signature, AlgorithmType::Rs384);
+type_level_Asymmetric_algorithm_type!(
+    sha2::Sha384,
+    rsa::pkcs1v15::SigningKey<sha2::Sha384>,
+    rsa::pkcs1v15::Signature,
+    AlgorithmType::Rs384
+);
+type_level_Asymmetric_algorithm_type!(
+    sha2::Sha384,
+    rsa::pkcs1v15::VerifyingKey<sha2::Sha384>,
+    rsa::pkcs1v15::Signature,
+    AlgorithmType::Rs384
+);
 
-type_level_Asymmetric_algorithm_type!(sha2::Sha512, rsa::pkcs1v15::SigningKey<sha2::Sha512>, rsa::pkcs1v15::Signature, AlgorithmType::Rs512);
-type_level_Asymmetric_algorithm_type!(sha2::Sha512, rsa::pkcs1v15::VerifyingKey<sha2::Sha512>, rsa::pkcs1v15::Signature, AlgorithmType::Rs512);
+type_level_Asymmetric_algorithm_type!(
+    sha2::Sha512,
+    rsa::pkcs1v15::SigningKey<sha2::Sha512>,
+    rsa::pkcs1v15::Signature,
+    AlgorithmType::Rs512
+);
+type_level_Asymmetric_algorithm_type!(
+    sha2::Sha512,
+    rsa::pkcs1v15::VerifyingKey<sha2::Sha512>,
+    rsa::pkcs1v15::Signature,
+    AlgorithmType::Rs512
+);
 
 // TODO: Ps256, Ps384, Ps512
 
-
-impl<HashAlgo, SignatureScheme, S> SigningAlgorithm for AsymmetricAuthentication<HashAlgo, SignatureScheme, S>
-    where
-        Self: TypeLevelAlgorithmType,
-        SignatureScheme: DigestSigner<HashAlgo, S>,
-        HashAlgo: Digest,
-        S: SignatureEncoding + std::fmt::Debug
+impl<HashAlgo, SignatureScheme, S> SigningAlgorithm
+    for AsymmetricAuthentication<HashAlgo, SignatureScheme, S>
+where
+    Self: TypeLevelAlgorithmType,
+    SignatureScheme: DigestSigner<HashAlgo, S>,
+    HashAlgo: Digest,
+    S: SignatureEncoding + std::fmt::Debug,
 {
     fn algorithm_type(&self) -> AlgorithmType {
         <Self as TypeLevelAlgorithmType>::algorithm_type()
@@ -132,13 +187,13 @@ impl<HashAlgo, SignatureScheme, S> SigningAlgorithm for AsymmetricAuthentication
     }
 }
 
-
-impl<HashAlgo, SignatureScheme, S> VerifyingAlgorithm for AsymmetricAuthentication<HashAlgo, SignatureScheme, S>
-    where
-        Self: TypeLevelAlgorithmType,
-        SignatureScheme: DigestVerifier<HashAlgo, S>,
-        HashAlgo: Digest,
-        S: SignatureEncoding
+impl<HashAlgo, SignatureScheme, S> VerifyingAlgorithm
+    for AsymmetricAuthentication<HashAlgo, SignatureScheme, S>
+where
+    Self: TypeLevelAlgorithmType,
+    SignatureScheme: DigestVerifier<HashAlgo, S>,
+    HashAlgo: Digest,
+    S: SignatureEncoding,
 {
     fn algorithm_type(&self) -> AlgorithmType {
         <Self as TypeLevelAlgorithmType>::algorithm_type()
@@ -152,23 +207,24 @@ impl<HashAlgo, SignatureScheme, S> VerifyingAlgorithm for AsymmetricAuthenticati
 
         let sig = S::try_from(signature).map_err(|_| Error::InvalidSignature)?;
 
-        self.0.verify_digest(hash, &sig).map_err(|_| Error::InvalidSignature)?;
+        self.0
+            .verify_digest(hash, &sig)
+            .map_err(|_| Error::InvalidSignature)?;
         Ok(true)
     }
 }
 
-
 impl<D> VerifyingAlgorithm for Hmac<D>
-    where
-        D: CoreProxy + TypeLevelAlgorithmType,
-        D::Core: HashMarker
-        + BufferKindUser<BufferKind=Eager>
+where
+    D: CoreProxy + TypeLevelAlgorithmType,
+    D::Core: HashMarker
+        + BufferKindUser<BufferKind = Eager>
         + FixedOutputCore
         + digest::Reset
         + Default
         + Clone,
-        <D::Core as BlockSizeUser>::BlockSize: IsLess<U256>,
-        Le<<D::Core as BlockSizeUser>::BlockSize, U256>: NonZero,
+    <D::Core as BlockSizeUser>::BlockSize: IsLess<U256>,
+    Le<<D::Core as BlockSizeUser>::BlockSize, U256>: NonZero,
 {
     fn algorithm_type(&self) -> AlgorithmType {
         D::algorithm_type()
@@ -182,16 +238,16 @@ impl<D> VerifyingAlgorithm for Hmac<D>
 }
 
 fn get_hmac_with_data<D>(hmac: &Hmac<D>, header: &str, claims: &str) -> Hmac<D>
-    where
-        D: CoreProxy,
-        D::Core: HashMarker
-        + BufferKindUser<BufferKind=Eager>
+where
+    D: CoreProxy,
+    D::Core: HashMarker
+        + BufferKindUser<BufferKind = Eager>
         + FixedOutputCore
         + digest::Reset
         + Default
         + Clone,
-        <D::Core as BlockSizeUser>::BlockSize: IsLess<U256>,
-        Le<<D::Core as BlockSizeUser>::BlockSize, U256>: NonZero,
+    <D::Core as BlockSizeUser>::BlockSize: IsLess<U256>,
+    Le<<D::Core as BlockSizeUser>::BlockSize, U256>: NonZero,
 {
     let mut hmac = hmac.clone();
     hmac.reset();
@@ -203,17 +259,16 @@ fn get_hmac_with_data<D>(hmac: &Hmac<D>, header: &str, claims: &str) -> Hmac<D>
 
 #[cfg(test)]
 mod tests {
+    use crate::algorithm::rust_crypto::AsymmetricAuthenticationBuilder;
     use crate::algorithm::{SigningAlgorithm, VerifyingAlgorithm};
-    use crate::algorithm::rust_crypto::{AsymmetricAuthenticationBuilder};
     use crate::error::Error;
     use hmac::{Hmac, Mac};
-    use p256::ecdsa::{SigningKey};
-    use p256::pkcs8::{DecodePrivateKey};
+    use p256::ecdsa::SigningKey;
+    use p256::pkcs8::DecodePrivateKey;
     use rsa::pkcs1::DecodeRsaPrivateKey;
+    use rsa::RsaPrivateKey;
     use sha2::Sha256;
-    use rsa::{RsaPrivateKey};
     use signature::Keypair;
-
 
     #[test]
     pub fn sign() -> Result<(), Error> {
@@ -243,7 +298,7 @@ mod tests {
 
     #[test]
     pub fn sign_asymmetric_ec() -> Result<(), Error> {
-        let private_key = include_str!("../../test/es256-private-2.pem");
+        let private_key = include_str!("../../test/es256-private.pem");
         let signing_key = SigningKey::from_pkcs8_pem(private_key).unwrap();
 
         let signer = AsymmetricAuthenticationBuilder::<Sha256>::build(signing_key);
@@ -261,10 +316,9 @@ mod tests {
 
     #[test]
     pub fn verify_asymmetric_ec() -> Result<(), Error> {
-        let private_key = include_str!("../../test/es256-private-2.pem");
+        let private_key = include_str!("../../test/es256-private.pem");
         let signing_key = SigningKey::from_pkcs8_pem(private_key).unwrap();
         let verifying_key = *signing_key.verifying_key();
-
 
         let verifer = AsymmetricAuthenticationBuilder::<Sha256>::build(verifying_key);
 
@@ -272,18 +326,18 @@ mod tests {
         let claims = "eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0";
         let signature = "dnpKzqJ1nYJc0BNCCwF0knVd5RQEX9abZrDETQrXfO_mZ9k8FMSMGb4Y8EF2OgAPw3HGCQ-gOn7TM6QPjXBvtA";
 
-        assert!(VerifyingAlgorithm::verify(&verifer, header, claims, signature)?);
-
+        assert!(VerifyingAlgorithm::verify(
+            &verifer, header, claims, signature
+        )?);
 
         Ok(())
     }
 
     #[test]
     pub fn sign_asymmetric_rsa_pkcs1v15() -> Result<(), Error> {
-        let private_key = include_str!("../../test/rs256-private-3.pem");
+        let private_key = include_str!("../../test/rs256-private.pem");
         let private_key = RsaPrivateKey::from_pkcs1_pem(private_key).unwrap();
         let signing_key = rsa::pkcs1v15::SigningKey::new(private_key);
-
 
         let signer = AsymmetricAuthenticationBuilder::<Sha256>::build(signing_key);
 
@@ -300,7 +354,7 @@ mod tests {
 
     #[test]
     pub fn verify_asymmetric_rsa_pkcs1v15() -> Result<(), Error> {
-        let private_key = include_str!("../../test/rs256-private-3.pem");
+        let private_key = include_str!("../../test/rs256-private.pem");
         let private_key = RsaPrivateKey::from_pkcs1_pem(private_key).unwrap();
         let signing_key = rsa::pkcs1v15::SigningKey::new(private_key);
         let verifying_key = signing_key.verifying_key();
@@ -311,7 +365,9 @@ mod tests {
         let claims = "eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0";
         let signature = "PI6Ji_Nh9-j8Q_BvtE3yJLvVOUugUuDMB3wau7gFN6ZMxg8M2_jlJhK8MnbPT-6_VgZBI63Lm_2KsCGSHvqiEPVK9io5yJdgSjpHvuzQnsbZyTvtKJrsZCeI_whuUPCKOlqoDLxzmYAegqYb_fmpkJ8odfhNdO3CGQIh0lL1hWKF27TBB095o5NfYm52OE2LjmTPltixGgoiLBsmrOCHiXscjAZrppG0cOB5q9BbZhYIQIbTykL9bTXqwe-QEaJefKp6lpp36M0pPsPTqMf00wNQZSl7_iFUf_4IPAiGQrUHgNqGzbBLJz_i651AfXlpL7u85vbjFUyIDqASN5csYw";
 
-        assert!(VerifyingAlgorithm::verify(&verifer, header, claims, signature)?);
+        assert!(VerifyingAlgorithm::verify(
+            &verifer, header, claims, signature
+        )?);
 
         Ok(())
     }
